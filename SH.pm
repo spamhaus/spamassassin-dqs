c260ba8d3f6c3e7ea6159f22af0d0cf8028bd795
Robert L Mathews <rob@tigertech.net>
2020-11-18 11:20:39 -0800

    Make check_sh_helo and check_sh_reverse strip any subdomains before comparing to $skip_domains or doing lookups
    These two functions currently do lookups on the found hostnames against ZRD and DBL, assuming the hostname is not in $skip_domains.
    
    However, if these include a subdomain like "sub.example.com", and "example.com" is already in (say) the From header, this will do  unnecessary extra lookups against the DBL and ZRD lists for "sub.example.com" in addition to the one for "example.com".
    
    In addition, "sub.example.com" is checked against $skip_domains, when it should actually compare "example.com".
    
    Stripping the subdomain fixes both problems.

--- SH.pm
+++ SH.pm
@@ -508,16 +508,17 @@ sub check_sh_helo {
   }
 
   my $helo = $lasthop->{helo};
-  if (!($skip_domains->{$helo})) {
-    dbg ("SHPlugin: (check_sh_helo) checking HELO (helo=$helo)");
-    my $lookup = $helo.".".$list;
+  my $helo_domain = $self->{'main'}->{'registryboundaries'}->uri_to_domain($helo);
+  if ($helo_domain and !($skip_domains->{$helo_domain})) {
+    dbg ("SHPlugin: (check_sh_helo) checking domain $helo_domain of HELO (helo=$helo)");
+    my $lookup = $helo_domain.".".$list;
     my $key = "SH:$lookup";
     my $ent = {
       key => $key,
       zone => $list,
       type => 'SH',
       rulename => $rulename,
-      addr => $helo,
+      addr => $helo_domain,
     };
     $ent = $pms->{async}->bgsend_and_start_lookup($lookup, 'A', undef, $ent, sub {
       my ($ent, $pkt) = @_;
@@ -665,22 +666,25 @@ sub check_sh_reverse {
 
   my $rdns = $lasthop->{rdns};
   if ($rdns) {
-    dbg ("SHPlugin: (check_sh_reverse) checking RDNS of the last untrusted relay (rdns=$rdns)");
-    if ((substr $rdns, -1) eq ".") { chop $rdns; }
-    my $lookup = $rdns.".".$list;
-    my $key = "SH:$lookup";
-    my $ent = {
-      key => $key,
-      zone => $list,
-      type => 'SH',
-      rulename => $rulename,
-      addr => $rdns,
-    };
-    $ent = $pms->{async}->bgsend_and_start_lookup($lookup, 'A', undef, $ent, sub {
-      my ($ent, $pkt) = @_;
-      $self->_finish_lookup($pms, $ent, $pkt, $subtest);
-    }, master_deadline => $pms->{master_deadline});
-    return 0;
+    my $rdns_domain = $self->{'main'}->{'registryboundaries'}->uri_to_domain($rdns);
+    if ($rdns_domain) {
+      dbg ("SHPlugin: (check_sh_reverse) checking domain $rdns_domain of RDNS of the last untrusted relay (rdns=$rdns)");
+      if ((substr $rdns_domain, -1) eq ".") { chop $rdns_domain; }
+      my $lookup = $rdns_domain.".".$list;
+      my $key = "SH:$lookup";
+      my $ent = {
+        key => $key,
+        zone => $list,
+        type => 'SH',
+        rulename => $rulename,
+        addr => $rdns_domain,
+      };
+      $ent = $pms->{async}->bgsend_and_start_lookup($lookup, 'A', undef, $ent, sub {
+        my ($ent, $pkt) = @_;
+        $self->_finish_lookup($pms, $ent, $pkt, $subtest);
+      }, master_deadline => $pms->{master_deadline});
+      return 0;
+    }
   }
 }
 
